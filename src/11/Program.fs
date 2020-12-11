155447d7bb92f8ea66c20e8ce3d760d885da7a59
Stephan Seitz <stephan.seitz@fau.de>
2020-12-11 10:44:44 +0100

    Only use a single update function for day 11

--- src/11/Program.fs
+++ src/11/Program.fs
@@ -7,7 +7,7 @@ type SeatState =
     | Floor
 
 let parseLine (line: string) =
-    line.ToCharArray()
+    line
     |> Seq.map (fun c -> match c with
                          | 'L' -> Empty
                          | '.' -> Floor
@@ -19,74 +19,49 @@ let swap (left : 'a byref) (right : 'a byref) =
   left <- right
   right <- temp
 
-let update (src: SeatState array array) (dst: SeatState array array) =
-    let mutable changes = 0
-    for y in 0..(src.Length-1) do
-        for x in 0..(src.[0].Length-1) do
-            try 
-                let mutable occupied = 0
-                let current = src.[y].[x]
-                for dy in -1..1 do
-                    for dx in -1..1 do
-                        try 
-                            if (dx = 0 && dy = 0) then
-                                ()
-                            else 
-                                match src.[y+dy].[x+dx] with
-                                    | Occupied -> occupied <- occupied + 1
-                                    | _ -> ()
-                        with _ -> ()
-                match (occupied, current) with
-                    | (0, Empty) -> dst.[y].[x] <- Occupied
-                                    changes <- changes + 1
-                    | (c, Occupied) when c >= 4 -> dst.[y].[x] <- Empty
-                                                   changes <- changes + 1
-                    | (_, current) -> dst.[y].[x] <- current
-            with _ -> ()
-    changes
+let findSeat1 (src: SeatState array array) x y dx dy =
+    src.[y+dy].[x+dx]
 
-let findSeat (src: SeatState array array) x y dx dy =
+let findSeat2 (src: SeatState array array) x y dx dy =
     let mutable nextSeat = Floor
     let mutable x_ = x
     let mutable y_ = y
     let width = src.[0].Length
     let height = src.Length
-    while nextSeat = Floor && x >= 0 && y >= 0 && x < width && y < height do
+    while nextSeat = Floor && x_+ dx >= 0 && y_ + dy >= 0 && x_ + dx < width && y_ + dy < height do
         x_ <- x_ + dx
         y_ <- y_ + dy
         nextSeat <- src.[y_].[x_]
     nextSeat
 
-let update2 (src: SeatState array array) (dst: SeatState array array) =
-    let mutable changes = 0
+let update findSeatFn neighborTolerance (src: SeatState array array) (dst: SeatState array array) =
+    let mutable changes = false
     for y in 0..(src.Length-1) do
         for x in 0..(src.[0].Length-1) do
-            try 
-                let mutable occupied = 0
-                let current = src.[y].[x]
-                for dy in -1..1 do
-                    for dx in -1..1 do
-                        try 
-                            if (dx = 0 && dy = 0) then
-                                ()
-                            else 
-                                match (findSeat src x y dx dy) with
-                                    | Occupied -> occupied <- occupied + 1
-                                    | _ -> ()
-                        with _ -> ()
-                match (occupied, current) with
-                    | (0, Empty) -> dst.[y].[x] <- Occupied
-                                    changes <- changes + 1
-                    | (c, Occupied) when c >= 5 -> dst.[y].[x] <- Empty
-                                                   changes <- changes + 1
-                    | (_, current) -> dst.[y].[x] <- current
-            with _ -> ()
+            let mutable occupied = 0
+            let current = src.[y].[x]
+            for dy in -1..1 do
+                for dx in -1..1 do
+                    try 
+                        if (dx = 0 && dy = 0) then
+                            ()
+                        else 
+                            match (findSeatFn src x y dx dy) with
+                                | Occupied -> occupied <- occupied + 1
+                                | _ -> ()
+                    with _ -> ()
+            match (occupied, current) with
+                | (0, Empty) -> dst.[y].[x] <- Occupied
+                                changes <- true
+                | (c, Occupied) when c >= neighborTolerance -> dst.[y].[x] <- Empty
+                                                               changes <- true
+                | (_, current) -> dst.[y].[x] <- current
     changes
 
 let run (input: SeatState array array) updateFn =
     let mutable dst = Array.init input.Length (fun y -> Array.copy input.[y])
     let mutable src = Array.init input.Length (fun y -> Array.copy input.[y])
-    while updateFn src dst > 0 do
+    while updateFn src dst do
         swap &src &dst
     dst
 
@@ -96,7 +71,7 @@ let parseInput (input: string) =
 
 let data = File.ReadAllText "input/11"
 let parsed = parseInput data
-let finalState = run parsed update
-let finalState2 = run parsed update2
+let finalState = run parsed (update findSeat1 4)
+let finalState2 = run parsed (update findSeat2 5)
 let solution1 = Array.sumBy (fun x -> x |> Seq.filter (fun elt -> elt = Occupied) |> Seq.length) finalState
 let solution2 = Array.sumBy (fun x -> x |> Seq.filter (fun elt -> elt = Occupied) |> Seq.length) finalState2
